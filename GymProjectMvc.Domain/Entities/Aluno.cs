58578e93186c5d1e2a9978a92b07d89353e4a5e0
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2022-07-01 19:52:51 -0300

    definindo os atributos das entidades e a navegação entre elas

--- GymProjectMvc.Domain/Entities/Aluno.cs
+++ GymProjectMvc.Domain/Entities/Aluno.cs
@@ -1,13 +1,17 @@
-﻿using CleanArchMvc.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GymProjectMvc.Domain.Entities
+﻿namespace GymProjectMvc.Domain.Entities
 {
-    public class Aluno : Entity
+    public sealed class Aluno : Entity
     {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public string? Cidade { get; set; }
+        public string? Bairro { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string Status { get; set; }
+
+        public int PlanoId { get; set; }
+        public Plano Plano { get; set; }
     }
 }
